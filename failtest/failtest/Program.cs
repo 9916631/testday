3349821ee326336c3382ed8989c326e8584b48fa
9916631 <ssmyth25@gmail.com>
2019-04-03 06:45:45 +1300

    test

---
+++ failtest/failtest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace failtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How tall are you?: ");
+            string tall = Console.ReadLine();
+
+        }
+    }
+}
